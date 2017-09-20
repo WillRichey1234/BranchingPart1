7d7323dce8dd09abcfd173156b4bd173faf8f589
WillRichey1234 <gsampson@vms.edu>
2017-09-20 11:46:29 -0600

    Branching Part 1

---
+++ BranchingPart1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BranchingPart1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            
+            string myValue = superSecretFormula();
+            Console.WriteLine(myValue);
+            Console.ReadLine();
+        }
+        //naming convention allows anyone who reads the code know that this
+        //is a private variable, use this for helper methods
+        private static string superSecretFormula()
+
+        {
+            //some cool stugg here
+            return "Hello World!";
+            // reuturn is the command to deliver it to the code that actually called it
+
+        }
+    }
+}
